3f0f410c53f7ad7f7be9c7ad5a3cf55e0c9cbd54
Рустам <miragewow5@gmail.com>
2022-07-07 14:08:15 +0300

    Fixed some bugs

--- src/main/java/FileReader.java
+++ src/main/java/FileReader.java
@@ -1,48 +1,43 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
 import javax.swing.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Map;
 
 public class FileReader {
-    public static void main(String[] parArgs)
-    {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileReader.class);
+
+    public static void main(String[] parArgs) throws IOException {
+        logger.trace("Process");
+
         JFileChooser fileopen = new JFileChooser();
         int ret = fileopen.showDialog(null, "Open file");
-        if (ret == JFileChooser.APPROVE_OPTION)
-        {
-            try
-            {
+        if (ret == JFileChooser.APPROVE_OPTION) {
+            try {
                 File file = fileopen.getSelectedFile();
                 FileInputStream fileInputStream = new FileInputStream(file);
 
                 {
                     Yaml yamlread = new Yaml();
                     Map<String, Object> data = yamlread.load(fileInputStream);
-                    if (data == null)
-                    {
+                    if (data == null) {
                         JOptionPane.showMessageDialog(null, "Empty file selected");
-                    }
-                    else
-                    {
-                        for (Map.Entry<String, Object> s: data.entrySet())
-                        {
-                            System.out.println(data);
+                        logger.error("empty file");
+                    } else {
+                        for (Map.Entry<String, Object> s : data.entrySet()) {
+                            System.out.println(s);
                         }
+                        logger.trace("its working");
                     }
                 }
-
-
-
-            }
-            catch (FileNotFoundException e)
-            {
+            } catch (FileNotFoundException e) {
                 throw new RuntimeException(e);
             }
+            logger.trace("Exit");
         }
-
-
     }
 }
+
